a84cbfcc8254aaa0a706777b5fd98ea2486c0d27
thegagne <1399002+thegagne@users.noreply.github.com>
2026-04-11 15:17:10 -0500

    Add OG tags, sitemap, robots.txt, LICENSE, dependabot

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ktext.dev',
   vite: {
     plugins: [tailwindcss()]
   },
-
+  integrations: [sitemap()],
   adapter: cloudflare()
 });
